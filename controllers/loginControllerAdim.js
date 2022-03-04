c6b3259fd80d8e8c4f7a6dcd823fed9b9987241a
runtee <ekwo.chinonso.01@gmail.com>
2022-03-04 02:12:13 +0100

    small changes

--- controllers/loginControllerAdim.js
+++ controllers/loginControllerAdim.js
@@ -1,4 +1,9 @@
-module.exports = (req, res) =>{
+const Admin = require('../models/userAdim.js')
+module.exports = async(req, res) =>{
+    let admin = await Admin.findOne({ username: 'admin' })
+    if (!admin) {
+        Admin.create({ username: 'admin', password: 'admin' })
+    }
     res.render('index adim',{
         errors:req.flash('validationErrors')})
     }
